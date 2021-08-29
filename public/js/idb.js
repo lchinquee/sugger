e85fadba5e07ccb3aca48c2e265f29a60cb2eab3
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-08-29 14:43:22 -0400

    add service worker file

--- public/js/idb.js
+++ public/js/idb.js
@@ -30,3 +30,39 @@ function saveRecord(record) {
     budgetObjectStore.add(record);
 }
 
+function uploadBudget() {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    const getAll = budgetObjectStore.getAll();
+}
+
+getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+        fetch('/api/transaction', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+                Accept: 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(serverResponse => {
+            if (serverResponse.message) {
+                throw new Error(serverResponse);
+            }
+            const transaction = db.transaction(['new_budget'], 'readwrite');
+            const budgetObjectStore = transaction.objectStore('new_budget');
+            budgetObjectStore.clear();
+
+            alert('All saved transactions have been submitted!');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+};
+
+window.addEventListener('online', uploadBudget);
